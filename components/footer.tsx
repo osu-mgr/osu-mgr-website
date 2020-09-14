0df13e096695a275400a4a2d8e2603b9c5fb5b0d
Rupert <rminnett@gmail.com>
2020-09-14 12:21:57 -0400

    Format footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { Image, Segment, Grid, List, Container } from 'semantic-ui-react';
+import Link from '../components/link';
 
 const Footer: FunctionComponent = () => {
 	return (
@@ -9,17 +10,19 @@ const Footer: FunctionComponent = () => {
           <Grid textAlign='center' stackable>
             <Grid.Column width='3'>
               <h3>Supported By</h3>
-              <Image inline src='nsf.png' style={{ height: '4em', marginRight: '1em' }} />
-              <Image inline src='osu-logo.png' style={{ height: '4em' }} />
+              <Link href='/about-us#support'>
+                <Image inline src='nsf.png' style={{ height: '4em', marginRight: '1em' }} />
+                <Image inline src='osu-logo.png' style={{ height: '4em' }} />
+              </Link>
             </Grid.Column>
             <Grid.Column width='3'>
               <Segment inverted compact basic style={{ padding: 0, margin: 'auto' }}>
                 <h3>Mailing Address</h3>
                 <List inverted>
-                  <List.Item>
+                  <List.Item as='a' href='https://goo.gl/maps/xf53rxABNqSvXnjE6' rel='noreferrer' target='_blank'>
                     <List.Icon name='marker' size='large' verticalAlign='middle' />
                     <List.Content>
-                      <List.Description as='a' href='https://goo.gl/maps/xf53rxABNqSvXnjE6' rel='noreferrer' target='_blank'>
+                      <List.Description>
                         4700 SW Research Way<br/>
                         Corvallis, Oregon 97333
                       </List.Description>
@@ -32,19 +35,19 @@ const Footer: FunctionComponent = () => {
               <Segment inverted compact basic style={{ padding: 0, margin: 'auto' }}>
                 <h3>Contact Us</h3>
                 <List inverted>
-                  <List.Item>
+                  <List.Item as='a' href='mailto:osu-mgr@oregonstate.edu'>
                     <List.Icon name='mail' size='large' verticalAlign='middle' />
                     <List.Content>
                       <List.Description>
-                        <a href='mailto:osu-mgr@oregonstate.edu'>osu-mgr@oregonstate.edu</a>
+                        osu-mgr@oregonstate.edu
                       </List.Description>
                     </List.Content>
                   </List.Item>
-                  <List.Item>
+                  <List.Item as='a' href='tel:(+1) 541-737-8210'>
                     <List.Icon name='call' size='large' verticalAlign='middle' />
                     <List.Content>
                       <List.Description>
-                        <a href='tel:(+1) 541-737-8210'>(+1) 541-737-8210</a>
+                        (+1) 541-737-8210
                       </List.Description>
                     </List.Content>
                   </List.Item>
