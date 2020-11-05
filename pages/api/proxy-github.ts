4306ac86215687b4ca4ba4aed387a39358a67da5
Rupert Minnett <rminnett@Gmail.com>
2020-11-05 16:40:26 -0500

    Return apiProxy result

--- pages/api/proxy-github.ts
+++ pages/api/proxy-github.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { apiProxy } from 'next-tinacms-github';
 
-export default (_req: NextApiRequest, res: NextApiResponse): void => {
+export default (_req: NextApiRequest, res: NextApiResponse): any => {
 	// eslint-disable-next-line no-console
 	console.log('proxy-github', _req.headers, _req.body, res);
-	apiProxy(process.env.SIGNING_KEY);
+	const proxy = apiProxy(process.env.SIGNING_KEY);
 	res.status(200).end();
+	return proxy;
 };
 
 //export default apiProxy(process.env.SIGNING_KEY);
