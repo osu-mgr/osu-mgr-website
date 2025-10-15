de751e298f225a45a6f024cc7a9ba56f2e273a75
Rupert Minnett <rminnett@gmail.com>
2025-10-15 00:38:14 -0700

    Fix features default error

--- components/blocks/features.tsx
+++ components/blocks/features.tsx
@@ -6,6 +6,7 @@ import { Icon } from "../util/icon";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { tinaField } from 'tinacms/dist/react'
 import { components, templates } from "../util/md-components";
+import { IconPickerInput } from "../fields/icon";
 
 export const Feature = ({ featuresColor, data }) => {
   return (
@@ -69,12 +70,32 @@ export const Features = ({ data }) => {
 
 const defaultFeature = {
   title: "Here's Another Feature",
-  text: "This is where you might talk about the feature, if this wasn't just filler text.",
+  text: {
+    type: "root",
+    children: [
+      {
+        type: "p",
+        children: [
+          {
+            type: "text",
+            text: "This is where you might talk about the feature, if this wasn't just filler text.",
+          },
+        ],
+      },
+    ],
+  },
   icon: {
     color: "",
-    style: "float",
+    style: "regular",
     name: "",
   },
+  actions: [
+    {
+      label: "Action Label",
+      link: "/",
+      icon: "BiRightArrowAlt"
+    }
+  ],
 };
 
 export const featureBlockSchema = {
@@ -82,7 +103,7 @@ export const featureBlockSchema = {
   label: "Features",
   ui: {
     defaultItem: {
-      items: [defaultFeature, defaultFeature, defaultFeature],
+      items: [defaultFeature, defaultFeature],
     },
   },
   fields: [
@@ -112,6 +133,47 @@ export const featureBlockSchema = {
           label: "Text",
           name: "text",
           templates
+        },
+        {
+          type: "object",
+          label: "Icon",
+          name: "icon",
+          fields: [
+            {
+              label: "Color",
+              name: "color",
+              type: "string",
+              options: [
+                { label: "Default", value: "" },
+                { label: "Blue", value: "blue" },
+                { label: "Teal", value: "teal" },
+                { label: "Green", value: "green" },
+                { label: "Red", value: "red" },
+                { label: "Pink", value: "pink" },
+                { label: "Purple", value: "purple" },
+                { label: "Orange", value: "orange" },
+                { label: "Yellow", value: "yellow" },
+              ],
+            },
+            {
+              label: "Style",
+              name: "style",
+              type: "string",
+              options: [
+                { label: "Regular", value: "regular" },
+                { label: "Float", value: "float" },
+                { label: "Circle", value: "circle" },
+              ],
+            },
+            {
+              label: "Name",
+              name: "name",
+              type: "string",
+              ui: {
+                component: IconPickerInput,
+              }
+            },
+          ],
         },
         actionsSchema
       ],
